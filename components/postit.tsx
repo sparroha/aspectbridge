39b812d89bed0a1efc75ae76c9fd0ffdb02f8b46
Keith <saphiresouls@aol.com>
2022-11-14 21:42:32 -0600

    .

--- components/postit.tsx
+++ components/postit.tsx
@@ -1,10 +1,11 @@
 export default function PostIt() { return <>
     <li id={'newlist'} className={'drag'}>
         click to move
-        <a href={'#'} onClick={"editNote(this,true);"}>
+        <a href={'#'}>
             <h2>New Note</h2>
             <p>content</p>
         </a>
     </li>
 </>}
+//onClick={"editNote(this,true);"}
     //<div id={'bulletin'}><PostIt /></div>
