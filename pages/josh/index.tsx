8d82374a1f9116387d054eddbaa88fd1ce0a6f55
Keith <saphiresouls@aol.com>
2022-11-26 01:57:31 -0600

    .

--- pages/josh/index.tsx
+++ pages/josh/index.tsx
@@ -4,8 +4,7 @@ import NavClient from './navigation/nav_client';
 import Head from "next/head";
 import Script from 'next/script';
 import Image from "next/image";
-import {Button, Col, Container, Form, Row} from "react-bootstrap";
-import Images, { Grass, Banner, Sunrise, Blue } from '../../components/images';
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 
 var activePage = "Home";
 export default function Main() {
@@ -21,17 +20,15 @@ export default function Main() {
             <Script src="js/script.js"></Script>
             <Script src="js/hebrew.js"></Script>
         </Head>
-        <Container fluid className={'logan'}>
-            <Row id='header' className='well-sm row p1 tcenter black-back h10'>
-                <Col sm={12} className={'text-warning tcenter h20 loganLogo'}>
+        <Container className={'logan'}>
+            <Row id='header' className='well-sm row p1 tcenter black-back h20'>
+                <Col sm={12} className={'text-warning tcenter grass'}>
                     <h1>Sunrise Landscapes</h1>
                     <NavIndex />
                 </Col>
             </Row>
             <Row id="content" className={"h70"}>
-                <Col sm={12}>
-                    {pageObj.jam.html}
-                </Col>
+                {pageObj.jam.html}
             </Row>
             <Row id="footer" className={"h10"}>
                 <Col sm={5} >
@@ -48,71 +45,50 @@ export default function Main() {
     </>
 }
 const pageObj = {
-    home: {
-        title: "Home",
-        html: <>
-            <Col md={1} id="nav1" className={"well-sm grey-back o5"}>
-            </Col>
-            <Col md={10} id='home' className={"well-sm white-back scroll"}>
-                <Row className={""}>
-                    <Col sm={12} id=""></Col>
-                </Row>
-                <Row className={""}>
-                    <Col md={12} id="homeContent" className={"tcenter black-font"}>
-                        <iframe id="homeContent" className={"scroll"} height="100%" width="100%" src ="" frameBorder={"0"}></iframe>
-                    </Col>
-                </Row>
-                <Row className={""}>
-                    <Col sm={3}></Col>
-                    <Col sm={6} id="">
-                        <Form id="tLit" className="vcenter tcenter">
-                            <Form.Group>
-                                <Form.Label>Input</Form.Label>
-                                <Form.Control  type="text" id="word" name="word" placeholder="Enter word" />
-                                <Form.Text className="text-muted"><h2>transliteration: </h2></Form.Text>
-                                <Form.Text className="text-muted"><h1 id="hbru"></h1></Form.Text>
-                            </Form.Group>
-                        
-                        </Form>
-                    </Col>
-                    <Col sm={3}></Col>
-                </Row>
-            </Col>
-            <Col md={1} id="nav2" className={"well-sm grey-back o5"}>
-            </Col>
-        </>
-    },
     jam: {
         title: "Jam",
         html: <>
-            <Row className={'h100'}>
-                <Col xs={6} sm={2} md={2} lg={2}  id="nav1" className={"well-sm grey-back o5"}>
-                    <Row className="h30">
-                        <Col sm={12} className="h100 grey-back p5">
-                            <Banner />
-                        </Col>
-                    </Row>
-                    <Row className="h70">
-                        <Col sm={12} className="h100 grey-back p5">
-                            <Grass />
-                        </Col>
-                    </Row>
-                </Col>
-                <Col xs={6} sm={8} md={9} lg={9} id="client-content" className="p5 grey-back o8">
-                            <h3 className="title">Client Content</h3>
-                </Col>
-                <Col xs={6} sm={2} md={1} lg={1}  id="nav2" className={"well-sm grey-back o5"}>
-                    <NavClient />
-                </Col>
-            </Row>
+            <Col xs={6} sm={2} md={2} lg={2}  id="nav1" className={"well-sm"}>
+                <Row className="">
+                    <Col sm={12} className="h100">
+                        <Card className={'text-white'}>
+                            <Card.Body className={'specles'}>
+                                <Card.Title>Banner</Card.Title>
+                                <Card.Text>banner text</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+                <Row className="">
+                    <Col sm={12} className="h100">
+                        <Card className={'text-white'}>
+                            <Card.Body className={'grass'}>
+                                <Card.Title className={'banner text-secondary'}>Grass</Card.Title>
+                                <Card.Text>banner text</Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                </Row>
+            </Col>
+
+            <Col xs={6} sm={8} md={9} lg={9} id="client-content" className="terrace">
+                        <h3 className="title">Client Content</h3>
+            </Col>
+
+            <Col xs={6} sm={2} md={1} lg={1}  id="nav2" className={"well-sm"}>
+                <NavClient />
+            </Col>
         </>
     },
     about: {
         title: "About",
         html: <>
-            <Row id='about' className='row m5'>
-                About Page
-            </Row>
+            <Card className={'text-white'}>
+                <Card.Body className={'mgrass'}>
+                    <Card.Title className={'banner text-secondary'}>About</Card.Title>
+                    <Card.Text>about text</Card.Text>
+                </Card.Body>
+            </Card>
         </>
     },
     nav: {
