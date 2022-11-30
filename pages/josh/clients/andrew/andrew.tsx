e65d5369574e8a2989157f029faf65a88a5ed6c6
Zypk <109366337+Zypk@users.noreply.github.com>
2022-11-30 07:43:16 -0600

    data entry
    Added andrew ashmore Fanny and..? nada mas

---
+++ pages/josh/clients/andrew/andrew.tsx
@@ -0,0 +1,5 @@
+export default function Andrew(){
+        return <>
+        this is cool
+                </>
+                }
---
+++ pages/josh/clients/ashmore/ashmore.tsx
@@ -0,0 +1,5 @@
+export default function Ashmore(){
+    return <>
+    this is cool
+            </>
+            }
