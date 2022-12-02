141b35926f1dfd1816dfa51adea22395d4e737e2
Keith <saphiresouls@aol.com>
2022-12-02 10:21:55 -0600

    .

--- components/dragdrop.tsx
+++ components/dragdrop.tsx
@@ -1,10 +1,22 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
  
 export default function DragDrop(){
   
   const dragItem = useRef();
   const dragOverItem = useRef();
   const [list, setList] = useState(['Item 1','Item 2','Item 3','Item 4','Item 5','Item 6']);
+  const [mouseX, setMouseX] = useState(0)
+  const [mouseY, setMouseY] = useState(0)
+  
+  useEffect(()=>{
+    window.onmousemove = handleMouseMove;
+    function handleMouseMove(e) {
+        e = e || window.event; // IE-ism
+        setMouseX(e.x)
+        setMouseY(e.y)
+    console.log(mouseX+"|"+mouseX)
+    }
+  });
  
   const dragStart = (e, position) => {
     dragItem.current = position;
