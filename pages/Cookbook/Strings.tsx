30c24a0a64ab7ae225b8adf017a44a09150b0835
Zypk <109366337+Zypk@users.noreply.github.com>
2022-12-09 22:25:13 -0600

    minorpatch

--- pages/Cookbook/Strings.tsx
+++ pages/Cookbook/Strings.tsx
@@ -1,4 +1,7 @@
-https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948
+
+export default function get angle(){
+    return <>
+    </>https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948
 
 <iframe scr=https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948/pubhtml?grid=0&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=A1:C2"width="500"height"600"></iframe>
 
