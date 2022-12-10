33185b40288b4a2c34725fcd932b4f3b157e4f40
Keith <saphiresouls@aol.com>
2022-12-10 03:13:02 -0600

    this is how this works

--- pages/Cookbook/Strings.tsx
+++ pages/Cookbook/Strings.tsx
@@ -1,4 +1,4 @@
-https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948
+export default function FFrame(){
+return <iframe src={'https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948/pubhtml?grid=0&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=A1:C2'} width="500" height="600"></iframe>
 
-<iframe scr=https://docs.google.com/spreadsheets/d/1Uu2Tg-YzOB31O2n2QH4xc1ZIGmJ45iR07w0xjDLutss/edit#gid=578441948/pubhtml?grid=0&amp;widget=false&amp;headers=false&amp;chrome=false&amp;range=A1:C2"width="500"height"600"></iframe>
-
+}
