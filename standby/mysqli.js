f5e725f5e30cb839215850d27cfb6816b7ef7a2c
Keith <saphiresouls@aol.com>
2022-12-17 09:18:29 -0600

    ,

--- standby/mysqli.js
+++ standby/mysqli.js
@@ -1,13 +1,4 @@
-<?php
-/*
-Work In Progress Global Use DATABASE Query Script
-*/
-/***Global VARS***/
-$GLOBALS['dbservername'] = "localhost";
-$GLOBALS['dbusername'] = "terrafor_admin";
-$GLOBALS['dbpassword'] = "sparrnet1";
-$GLOBALS['dbname'] = "terrafor_net";
-$GLOBALS['dbverbose'] = false;
+
 /***FUNCTIONS***
  * 
  * //String $x; colomn names
@@ -35,212 +26,80 @@ $GLOBALS['dbverbose'] = false;
  *deleteFromTableWhereArray($table,$array){}
  */
 
-
-function conn(){
-	$dbservername = $GLOBALS['dbservername'];
-	$dbusername = $GLOBALS['dbusername'];
-	$dbpassword = $GLOBALS['dbpassword'];
-	$dbname = $GLOBALS['dbname'];
-	$conn = new mysqli($dbservername, $dbusername, $dbpassword, $dbname);
-	return $conn;
-}
 //$array => [0] {[key]=value, [val]=value}
 //returns array;
-function selectXFromTableWhereArray($x,$table,$array){
-	$conn = conn();
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	if($GLOBALS['dbverbose'])echo '<br>successfull connection ['.$conn->connect_error.']<br>';
+export function selectXFromTableWhereArray($x,$table,$array){
 	//build $sql string
-	$sql =  "SELECT ".$x." FROM ".$table;
+	$sql =  "SELECT "+$x+" FROM "+$table;
 	if(isset($array)){
-		$sql =  $sql." where ";
+		$sql =  $sql+" where ";
 		for($y=0;$y<count($array);$y++){
 			if(substr_compare('^',$array[$y]['val'],0,1,true)!=0){
-				$array[$y]['val']='^'.$array[$y]['val'].'$';
+				$array[$y]['val']='^'+$array[$y]['val']+'$';
 			}
-			$sql = $sql. $array[$y]['key']." RLIKE '".$array[$y]['val']."' and ";
+			$sql = $sql. $array[$y]['key']+" RLIKE '"+$array[$y]['val']+"' and ";
 		}
 		$sql = rtrim($sql,"and ");
 	}
-	//query $sql string
-	$result = $conn->query($sql);
-	
-	//build $ret array
-	if ($result->num_rows > 0) {
-		// output data of each row
-		$ret = array();
-		while($row = $result->fetch_assoc()) {
-			array_push($ret,$row);
-		}
-	} else {
-		if($GLOBALS['dbverbose'])echo "mysqli error checking selectXFromTableWhereArray: ".mysqli_connect_errno().'+error: '.$conn->error;
-	}
-	$conn->close();
-	return $ret;
+	return $sql;
 }
 //$array => [0] {[key]=value, [val]=value}
 //returns boolean;
-function insertIntoTableValuesOfArray($table,$array){
-	$conn = conn();
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	//seperate key pairs
-	$keyarray = array();
-	$valarray = array();
+export function insertIntoTableValuesOfArray($table,$array){
+	$sql = "INSERT INTO "+$table+" (";
 	for($y=0;$y<count($array);$y++){
-		//echo $array[$y]['key'];
-		array_push($keyarray,$array[$y]['key']);
-		//echo $array[$y]['val'];
-		array_push($valarray,$array[$y]['val']);
+		$sql = $sql+$keyarray[$y]+", ";
 	}
-	//build $sql string
-	$sql = "INSERT INTO ".$table." (";
+	$sql = rtrim($sql,", ")+") VALUES (";
 	for($y=0;$y<count($array);$y++){
-		$sql = $sql.$keyarray[$y].", ";
+		$sql = $sql+"'"+$valarray[$y]+"', ";
 	}
-	$sql = rtrim($sql,", ").") VALUES (";
-	for($y=0;$y<count($array);$y++){
-		$sql = $sql."'".$valarray[$y]."', ";
-	}
-	$sql = rtrim($sql,", ").")";
-	//query $sql string
-	$result = $conn->query($sql);
-	//set $ret val
-	
-	if ($result){
-	 	$ret = true;
-    }else{
-		if($GLOBALS['dbverbose'])echo "mysqli error checking insertIntoTableValuesOfArray: ".mysqli_connect_errno().'+error: '.$conn->error;
-		$ret = $conn->error;
-	}
-	$conn->close();
-	return $ret;
+	$sql = rtrim($sql,", ")+")";
+	return $sql;
 }
 //$array => [0] {[key]=value, [val]=value}
 //returns boolean;
-function updateTableSetArrayWhereKeyVal($table,$setarray,$selectarray){
-	$conn = conn();
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	//build $sql string
-	$sql = "UPDATE ".$table." SET ";
-	for($y=0;$y<count($setarray);$y++){
-		$sql = $sql.$setarray[$y]['key']."='".$setarray[$y]['val']."', ";
-	}
-	$sql = rtrim($sql,", ")." WHERE ";
+export function updateTableSetArrayWhereKeyVal($table,$setarray,$selectarray){
+	$sql = rtrim($sql,", ")+" WHERE ";
 	for($y=0;$y<count($selectarray);$y++){
 		if(substr_compare('^',$selectarray[$y]['val'],0,1,true)!=0){
-			$selectarray[$y]['val']='^'.$selectarray[$y]['val'].'$';
+			$selectarray[$y]['val']='^'+$selectarray[$y]['val']+'$';
 		}
-		$sql = $sql. $selectarray[$y]['key']." RLIKE '".$selectarray[$y]['val']."' and ";
+		$sql = $sql+$selectarray[$y]['key']+" RLIKE '"+$selectarray[$y]['val']+"' and ";
 	}
 	$sql = rtrim($sql,"and ");
-	//query $sql string
-	$result = $conn->query($sql);
-	//set $ret val
-	
-	if ($result){
-	 	$ret = true;
-    }else{
-		if($GLOBALS['dbverbose'])echo "mysqli error checking insertIntoTableValuesOfArray: ".mysqli_connect_errno().'+error: '.$conn->error;
-		$ret = false;
-	}
-	$conn->close();
-	return $ret;
+	return $sql;
 }
-function alterTableTAuto_Increment($table,$num){
-	if($num<=0){
-		//auto detect
-		//$s = "SHOW KEYS FROM ".$table." WHERE Key_name = 'PRIMARY'";
-		//selectMaxXAsNameFromTableWhereArray($x,$table,$array);
-		return false;
-	}
-	$conn = conn();
-	
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	//build $sql string
-	$sql = "ALTER TABLE ".$table." AUTO_INCREMENT=".$num;
+export function alterTableTAuto_Increment($table,$num){
+	$sql = "ALTER TABLE "+$table+" AUTO_INCREMENT="+$num;
 	//query $sql string
-	$result = $conn->query($sql);
-	//set $ret val
-	
-	if ($result){
-	 	$ret = true;
-    }else{
-		if($GLOBALS['dbverbose'])echo "mysqli error checking alterTableTAuto_Increment: ".mysqli_connect_errno().'+error: '.$conn->error;
-		$ret = false;
-	}
-	$conn->close();
-	return $ret;
+	return $sql;
 }
-function selectMaxXAsNameFromTableWhereArray($x,$table,$array){
-	$conn = conn();
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	if($GLOBALS['dbverbose'])echo '<br>successfull connection ['.$conn->connect_error.']<br>';
-	//build $sql string
-	$sql =  "SELECT MAX(".$x.") AS ".$x." FROM ".$table;
+export function selectMaxXAsNameFromTableWhereArray($x,$table,$array){
+	$sql =  "SELECT MAX("+$x+") AS "+$x+" FROM "+$table;
 	if(count($array)>0){
-		$sql= $sql." WHERE ";
+		$sql= $sql+" WHERE ";
 		for($y=0;$y<count($array);$y++){
 			if(substr_compare('^',$array[$y]['val'],0,1,true)!=0){
-				$array[$y]['val']='^'.$array[$y]['val'].'$';
+				$array[$y]['val']='^'+$array[$y]['val']+'$';
 			}
-			$sql = $sql. $array[$y]['key']." RLIKE '".$array[$y]['val']."' and ";
+			$sql = $sql. $array[$y]['key']+" RLIKE '"+$array[$y]['val']+"' and ";
 		}
 		$sql = rtrim($sql,"and ");
 	}
-	//query $sql string
-	$result = $conn->query($sql);
-	
-	//build $ret array
-	if ($result->num_rows > 0) {
-		// output data of each row
-		$ret = array();
-		while($row = $result->fetch_assoc()) {
-			array_push($ret,$row);
-		}
-	} else {
-		if($GLOBALS['dbverbose'])echo "mysqli error checking selectMaxXAsNameFromTableWhereArray: ".mysqli_connect_errno().'+error: '.$conn->error;
-	}
-	$conn->close();
-	return $ret;
+	return $sql;
 }
-function deleteFromTableWhereArray($table,$array){
-	$conn = conn();
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	}
-	if($GLOBALS['dbverbose'])echo '<br>successfull connection ['.$conn->connect_error.']<br>';
-	//build $sql string
-	$sql =  "DELETE FROM ".$table;
+export function deleteFromTableWhereArray($table,$array){
+	$sql =  "DELETE FROM "+$table;
 	if(count($array)>0){
-		$sql= $sql." WHERE ";
+		$sql= $sql+" WHERE ";
 		for($y=0;$y<count($array);$y++){
 			if(substr_compare('^',$array[$y]['val'],0,1,true)!=0){
-				$array[$y]['val']='^'.$array[$y]['val'].'$';
+				$array[$y]['val']='^'+$array[$y]['val']+'$';
 			}
-			$sql = $sql. $array[$y]['key']." RLIKE '".$array[$y]['val']."' and ";
+			$sql = $sql+$array[$y]['key']+" RLIKE '"+$array[$y]['val']+"' and ";
 		}
 		$sql = rtrim($sql,"and ");
 	}
-	//query $sql string
-	$result = $conn->query($sql);
-	
-	if ($result){
-	 	$ret = true;
-    }else{
-		if($GLOBALS['dbverbose'])echo "mysqli error checking deleteFromTableWhereArray: ".mysqli_connect_errno().'+error: '.$conn->error;
-		$ret = false;
-	}
-	$conn->close();
-	return $ret;
+	return $sql;
 }
-?>
