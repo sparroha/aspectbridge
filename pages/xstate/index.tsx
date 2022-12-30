ff57f3d711d654f63b31f0254dc1084b6480b08e
Keith <saphiresouls@aol.com>
2022-12-30 01:45:44 -0600

    .
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .
    
    .

--- pages/xstate/index.tsx
+++ pages/xstate/index.tsx
@@ -1,6 +1,13 @@
 import { createMachine } from "xstate"
 export default function SM(){
-    const xsm = createMachine({
-        initial: 'ready'
-    })
+    const xsm = 
+/** @xstate-layout N4IgpgJg5mDOIC5gF8A0IB2B7CdGgAoBbAQwGMALASwzAEp8QAHLWKgFyqw0YA9EAjADoArAHYATCIAMATjEA2EQqnoAnogC0ADiETZ0sdoViALAGYBC09JXI0IYuWq06QgE5gSEDUmasOLh4-fgRTWVFJGXklFRF1RCMhAVkFFVMxERFtEVlzbXt7IA */
+createMachine({
+    states: {
+        ready: {}
+    },
+
+    initial: "ready"
+})
+    return <>{xsm.initial}</>
 }
