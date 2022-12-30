1595e1ec5fe4ddb13c30a72ca0f778a66866dc86
Keith <saphiresouls@aol.com>
2022-12-29 23:17:01 -0600

    .

--- components/domain.tsx
+++ components/domain.tsx
@@ -6,27 +6,21 @@ export default function useDomainRoot(){
     const [domain, setDomain] = useState('')
     useEffect(() => {
         setDomain(/:\/\/([^\.]+)/.exec(window.location.href)[1])
-        //console.log('@useDomainRoot('+domain+'||'+window.location.href+')')
-    }, []);
-    return ()=> {
-        /**
-         * In this configuration, the only accessable pages are the root [...slug] pages
-         */
-        if(domain != 'localhost:3000'){
-            //if loading from 'aspectbridge.' or 'www.' then redirect to [...aspect]
-            if(domain == "aspectbridge" || "www"){
-                useEffect(() => {
-                    router.push({pathname: '/bridge/dashboard', query: router.query})
-                }, []);
-            }
-            //if loading from 'logan.' then redirect to [josh/[...client]...aspect]
-            else if(domain == "logan"){
-                useEffect(() => {
-                    router.push({pathname: '/josh/dashboard', query: router.query})
-                }, []);
+        console.log('@useDomainRoot('+domain+'||'+window.location.href+')')
+        
+        return ()=> {
+            /**
+             * In this configuration, the only accessable pages are the root [...slug] pages
+             */
+            if(domain != 'localhost:3000'){
+                //if loading from 'aspectbridge.' or 'www.' then redirect to [...aspect]
+                if(domain == "aspectbridge" || "www"){router.push({pathname: '/bridge/dashboard', query: router.query})}
+                //if loading from 'logan.' then redirect to [josh/[...client]...aspect]
+                else if(domain == "logan"){router.push({pathname: '/josh/dashboard', query: router.query})}
+                alert(domain)
             }
         }
-    }
+    }, [domain]);
 }
 export function getDomain(){
     const [domain, setDomain] = useState('')
