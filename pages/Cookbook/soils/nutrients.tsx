02580efaa7e870d818a5b3cee63d76e1e453f295
Zypk <109366337+Zypk@users.noreply.github.com>
2023-02-04 19:18:52 -0600

    last few changes

--- pages/Cookbook/soils/nutrients.tsx
+++ pages/Cookbook/soils/nutrients.tsx
@@ -6,26 +6,6 @@ export default function Nutrients (){
 }
 
 //function=nutrients [10,10]
-const nitrogenObj ={protoncount:"7"}
-const potassiumObj ={protoncount:"19"}
-const phosphorusObj ={protoncount:"15"}
-//primary nutrients = nitrogen,phosphorus,potassium
-
-const sulferObj ={protoncount:"16"}
-const magnesiumObj ={protoncount:"12"}
-const calciumObj ={protoncount:"20"}
-//secondary
-
-const ironObj ={protoncount:"26"}
-const boronObj ={protoncount:"5"}
-const manganeseObj ={protoncount:"25"}
-const zincObj ={protoncount:"30"}
-const molybdenumObj ={protoncount:"42"}
-const copperObj ={protoncount:"29"}
-const cobaltObj ={protoncount:"27"}
-const chlorineObj ={protoncount:"17"}
-const nickleObj ={protoncount:"28"}
-//macro
 
 //function=drainage
 //function=retention 
