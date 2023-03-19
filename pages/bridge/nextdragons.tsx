c43d9eca4e0ed1c93770671ad1059b1a6787e5f1
Keith <saphiresouls@aol.com>
2023-03-18 20:34:33 -0500

    .

--- pages/bridge/nextdragons.tsx
+++ pages/bridge/nextdragons.tsx
@@ -5,6 +5,12 @@ import requestIp from 'request-ip';
 import useLog from "../../components/conlog";
 import { ProfileByIp } from "../login/[userlogin]";
 
+export const portcontrol = {
+    fontSize: '10px',
+    margin: '0px',
+    padding: '0px',
+    text: 'center',
+}
 export const control = {
     fontSize: '10px',
     width: '48px',
@@ -73,19 +79,19 @@ function MapSettings({map, sSP}){
             <option key={1} value={1}>1</option>
             <option key={2} value={2}>2</option>
         </select>
-        <Form id={'loginForm'} onSubmit={(event) => {event.preventDefault();
+        <Form id={'loginForm'} style={portcontrol} onSubmit={(event) => {event.preventDefault();
                 if(typeof map.M[z] === 'undefined') return;
                 if(typeof map.M[z][x] === 'undefined') return;
                 if(typeof map.M[z][x][y] === 'undefined') return;
                 sSP({x: x, y: y, z: z})
             }} >
-            <Form.Group controlId="formEmail">
-                <Form.Label>X</Form.Label>
-                <Form.Control required type="number" value={x} onChange={(e)=>setX(parseInt(e.target.value))}/>
-                <Form.Label>Y</Form.Label>
-                <Form.Control required type="number" value={y} onChange={(e)=>setY(parseInt(e.target.value))}/>
-                <Form.Label>Z</Form.Label>
-                <Form.Control required type="number" value={z} onChange={(e)=>setZ(parseInt(e.target.value))}/>
+            <Form.Group style={portcontrol} controlId="formEmail">
+                <Form.Label>Longitude</Form.Label>
+                <Form.Control style={portcontrol} required type="number" min={1} value={x+1} onChange={(e)=>setX(parseInt(e.target.value)-1)}/>
+                <Form.Label>Latitude</Form.Label>
+                <Form.Control style={portcontrol} required type="number" min={1} value={y+1} onChange={(e)=>setY(parseInt(e.target.value)-1)}/>
+                <Form.Label>Altitude</Form.Label>
+                <Form.Control style={portcontrol} required type="number" min={1} value={z+1} onChange={(e)=>setZ(parseInt(e.target.value)-1)}/>
             </Form.Group>
             <Button type="submit" >Teleport</Button>
         </Form>
