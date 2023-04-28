32745597cd2972c8b78c873bc31191cf646736f9
Keith <anthymngalaris@gmail.com>
2023-04-28 16:52:21 -0500

    Update dice.tsx
    revisualize

--- components/dice.tsx
+++ components/dice.tsx
@@ -48,8 +48,67 @@ export default function DiceWidget({udr}: {udr: Function}){
             borderRadius: '25%',
             border: '5px outset lightgray',
         }} onClick={() => setValue(Math.floor(rand()*sides)+1)}>{value}</button>
+      <DiceCompact udr={udr}/>
     </div>
 }
+function DiceCompact({udr}){
+    const {sides, speed, rand, 
+           value, setRoller, 
+           selectSides, setValue, 
+           setSpeed} = udr()
+    const dice = [2,4,6,8,10,12,20,100]
+    useEffect(() => {
+        if(setValue&&sides==0){
+            let r = dice[Math.floor(rand()*8)]
+            selectSides(r)
+            setValue(Math.floor(rand()*r)+1)
+        }
+    }, [setValue])
+
+    return <div>
+        <button
+            style={{
+                height: '40px',
+                width: '40px',
+                color: 'blue',
+                backgroundColor: 'silver',
+                borderRadius: '25%',
+                border: '5px outset lightgray',
+            }}
+            onClick={() => setValue(
+            Math.floor(rand()*sides)+1)}
+        ><Row>
+            <Col xs={4}><select value={sides} onChange={e => selectSides(Number(e.target.value))}>
+            {dice.map((side, index) => <option key={index} value={side}>{side}</option>)}
+        </select></Col>
+
+        <Col xs={4}></Col>
+
+        <Col xs={4}><input type="checkbox" onChange={e => e.target.checked ? setRoller('true') : setRoller('false')} /> Auto
+        </Col>
+        </Row>
+        <Row><Col xs={12}>{value}</Col></Row>
+        
+
+        <Col xs={4}>
+Sides:<input style={{
+            borderRadius: '50%',
+        }} type="number" min={2} max={100} defaultValue={sides} onChange={e => selectSides(Number(e.target.value))} />
+        </Col>
+        <Col xs={4}></Col>
+        <Col xs={4}>
+Speed:<input style={{
+            borderRadius: '50%',
+        }} type="number" min={1} max={10} defaultValue={speed} onChange={e => setSpeed(Number(e.target.value))} />
+        small is fast
+        </Col>
+
+
+         
+        </button>
+    </div>
+}
+
 export function useDiceRoll(props: diceInitProps): diceProps{
     const rand = ()=>Math.random();
     const [sides, selectSides] = useState(props.sides || 0)
