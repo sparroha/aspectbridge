c6da535418e1c68aa2697011bf0e7fc697568a0d
Keith <saphiresouls@aol.com>
2023-04-30 04:44:47 -0500

    user save

---
+++ pages/api/cost/[request].ts
@@ -0,0 +1,64 @@
+import sql from "../../../lib/,base/sql"
+
+export default async function cost(req, res) {
+    const { request } = req.query
+    const { username, send, time, message, access, coin, income } = req.body
+    const method = req.method
+    if (method === 'POST') {try {
+        switch (request) {
+          case 'deleteuser':
+            await sql`DELETE FROM aspect_cost_users_ WHERE username = ${username};`
+            res.status(200).json({ alert: 'user deleted' })
+            break
+          case 'users'://aka update user
+            await sql`INSERT INTO aspect_cost_users_ (username, last_active, coin, income) VALUES (${username}, NOW(), ${coin}, ${income}) ON DUPLICATE KEY UPDATE last_active = NOW(), coin = ${coin}, income = ${income};`
+            res.status(200).json({ alert: 'user added' })
+            break
+          default:
+            res.status(400).json({
+              alert: 'Invalid request',
+              request: request,
+              method: method,
+              username: username,
+              coin: coin,
+              income: income,
+            })
+        }
+      } catch (error) {
+        res.status(500).json({ alert: 'Server error', body: req.body, username: username, send: send, error: error })
+      }
+    } else {
+      try {
+        switch (request) {
+          case 'users':
+            const users = !username?await sql`SELECT * FROM aspect_cost_users_ ORDER BY last_active DESC;`:
+                              await sql`SELECT * FROM aspect_cost_users_ WHERE username = ${username};`
+            res.status(200).json(users)
+            break
+          case 'resetusers':
+            await sql`DROP TABLE IF EXISTS aspect_cost_users_;`
+            await sql`CREATE TABLE IF NOT EXISTS aspect_cost_users_ (
+                id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+                username VARCHAR(100) NOT NULL UNIQUE,
+                last_active DATETIME NOT NULL,
+                coin INT NOT NULL DEFAULT 0,
+                income INT NOT NULL DEFAULT 0
+            );`
+            res.status(200).json({ alert: 'users reset' })
+            break
+          default:
+            res.status(400).json({
+              alert: 'Invalid request',
+              request: request,
+              method: method,
+              username: username,
+              coin: coin,
+              income: income,
+            })
+        }
+      } catch (error) {
+        res.status(500).json({ alert: 'Server error', body: req.body, username: username, income: income, error: error })
+      }
+    }
+  }
+  
--- pages/cost/index.tsx
+++ pages/cost/index.tsx
@@ -1,49 +1,94 @@
+import { GetServerSideProps } from "next";
 import { useEffect, useMemo, useState } from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import requestIp from 'request-ip';
+import { Profile } from "../login/[userlogin]";
+import { LoginNav } from "../login/[userlogin]";
 
 
-export default function Cost() {
+export default function Cost(props) {
     const [coin, setCoin] = useState(0)
     const [income, setIncome] = useState(0)
+    const [user, setUser] = useState(null)
+    const [update, setUpdate] = useState(false)
+
+    useEffect(() => {
+        const player = fetch('/api/cost/users?username='+user?.username)
+        .then((res)=>res.json())
+        .then((data)=>{
+            console.log(data)
+            coin==0?setCoin(data[0].coin):null
+            income==0?setIncome(data[0].income):null
+        }).catch((e)=>{console.log(e)})
+    }, [user])
+
+    useEffect(() => {
+        if(update)updatePlayer(user, coin, income)
+        console.log('update')
+        return setUpdate(false)
+    }, [update])
+
 
     useEffect(() => {
         const interval = setInterval(() => {
             setCoin((c)=>{return c+income})
         }, 1000);
         return () => clearInterval(interval);
-        }, [income]);
+    }, [income]);
         
-    const RenderButton = ()=>{
+    function RenderButton(){
         const buttons = []
-        for(let i=1;i<10;i++){
+        for(let i=1;i<=Math.floor(Math.log10(coin));i++){
             buttons.push(
-                coin>=Math.pow(10,i)?//if current coin is greater than 10^i then display button
-                <><button key={i} onClick={()=>{
+                <><Button onClick={()=>{
                     setIncome((p)=>{return p+Math.pow(10,i)/10})//set income to current income + (i-1)*10
                     setCoin((c)=>{return c-Math.pow(10,i)})//set coin to current coin - 10^i
-                }}>+{Math.pow(10,i)/10} income {'(-'}{Math.pow(10,i)}{' coin)'}</button><br/></>:
-                null
+                    setUpdate(true)
+                }}>+{Math.pow(10,i)/10} income {'(-'}{Math.pow(10,i)}{' coin)'}</Button><br/></>
             )
         }
         return <Row>
             {buttons.map((b, i)=>{
-                return <Col xs={5} sm={4} md={3} lg={2}>{b}</Col>
+                return <Col key={i+1} xs={5} sm={4} md={3} lg={2}>{b}</Col>
             })}
         </Row>
     }
     return (
         <Container>
+            <LoginNav user={user} homepage={props.homepage || 'cost'} />
+            <Profile ip={props.ip} setUser={setUser}/>
             <Row style={{marginTop: '20px'}}>
                 <Col xs={12} sm={4}><h1>Cost</h1></Col>
                 <Col xs={12} sm={6}><h3>Basic Idle Clicker</h3></Col>
                 <Col xs={0} sm={2}></Col>
             </Row>
             <Row>
-                <Col xs={12} sm={4}><button onClick={()=>setCoin((c)=>{return ++c})}>Increment coin by 1</button></Col>
+                <Col xs={12} sm={4}><Button onClick={()=>{setCoin((c)=>{return ++c});setUpdate(true)}}>Increment coin by 1</Button></Col>
                 <Col xs={12} sm={4}><label>Coin: {coin}</label></Col>
                 <Col xs={12} sm={4}><label>income: {income}</label></Col>
             </Row>
             <RenderButton />
         </Container>
     );
+}
+function updatePlayer(user, coin, income){
+    console.log('updatePlayer')
+    fetch('/api/cost/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: user.username,
+            coin: coin,
+            income: income
+        })
+    }).then((res)=>res.json())
+    .then((data)=>{console.log(data)})
+    .catch((e)=>{console.log(e)})
+}
+export const getServerSideProps: GetServerSideProps = async (context) => {
+    const query = context.query
+    const ip = await requestIp.getClientIp(context.req)
+    return {props: {ip: ip}} 
 }
