1d8f82d32cd1f0c12a27308eb8f618ad261dde6b
Keith <saphiresouls@aol.com>
2023-05-02 19:49:12 -0500

    .

--- components/dialog.tsx
+++ components/dialog.tsx
@@ -1,20 +1,32 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { Button } from "react-bootstrap"
 
+/**
+ * 
+ * @param props  id: string, title: string, content: string, open: string, close: string, style: object
+ * @returns 
+ */
 export default function Dialog(props?){
-    const {id, title, content, open, close} = props
+    const {id, title, content, open, close, style} = props
+    const [active, setActive] = useState(false)
 
     useEffect(()=>{
         const openModal = document.querySelector('#open'+id)
         const closeModal = document.querySelector('#close'+id)
         const modal: HTMLDialogElement = document.querySelector('#modal'+id)
-        openModal.addEventListener('click', ()=>{modal.showModal()})
-        closeModal.addEventListener('click', ()=>{modal.close()})
-    },[])
+        const openF = ()=>{if(!active){modal.showModal();setActive(true)}}
+        const closeF = ()=>{if(active){modal.close();setActive(false)}}
+        openModal.addEventListener('click', openF)
+        closeModal.addEventListener('click', closeF)
+        return ()=>{
+            openModal.removeEventListener('click', openF)
+            closeModal.removeEventListener('click', closeF)
+        }
+    },[active])
 
     return<>
         <Button id={'open'+id} data-open-modal>{open?open:'Open'}</Button>
-        <dialog id={'modal'+id} data-modal>
+        <dialog style={{borderRadius: '25px', border: '5px outset silver', textAlign: 'center',...style}} id={'modal'+id} data-modal>
             <h2>{title?title:'Modal'}</h2>
             <p>{content?content:'Modal content'}</p>
             <Button id={'close'+id} data-close-modal>{close?close:'Close'}</Button>
