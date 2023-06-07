e53717de7691802b6ea3c577b5965caad2d547a4
Keith <940398+sparroha@users.noreply.github.com>
2023-06-06 22:12:17 -0500

    .

--- components/sliders.tsx
+++ components/sliders.tsx
@@ -1,110 +1,123 @@
 import { useEffect, useRef, useState } from "react";
 import Dialog from "./dialog";
-import { Col, Row } from "react-bootstrap";
+import { Col, Container, Row } from "react-bootstrap";
 
 export default function CssSlidersWrapper(props) {
-  //{children}
-  const controlStyle = {
-    top: "0vh",
-    right: "0vw",
-    zIndex: 10,
-  };
-  const [state, setState] = useState({});
-  const styleRef = useRef({
-    top: "0vh",
-    left: "0vw",
-    width: '100vw',
-    height: '100vh',
-    opacity: '100%',
-    borderRadius: '0%',
-    borderWidth: '0%',
-    fontSize: '100%',
-    margin: '0%',
-    padding: '0%',
-    color: 'black',
-    display: 'flex',
-    //alignItems: 'center',
-    //justifyContent: 'center',
-    backgroundColor: 'gray',
-    borderColor: 'black',
-})
+    //{children}
+    const controlStyle = {
+        top: "0vh",
+        left: "0vw",
+        zIndex: 10,
+    };
+    const [state, setState] = useState({});
+    const styleRef = useRef({
+        top: "0vh",
+        left: "0vw",
+        width: '100vw',
+        height: '100vh',
+        opacity: '100%',
+        borderRadius: '0%',
+        borderWidth: '0%',
+        //fontSize: '100%',
+        margin: '0%',
+        padding: '0%',
+        color: 'black',
+        display: 'flex',
+        //alignItems: 'center',
+        //justifyContent: 'center',
+        backgroundColor: 'gray',
+        borderColor: 'black',
+        ...props.style
+    })
 
-  /*const [content, setContent] = useState(<></>)
-    useEffect(()=>{
-        setContent(<>
-            
-        </>)
-    },[])*/
+    useEffect(() => {
+        props.setStyle(props.id, {...styleRef.current})
+        console.log(props.id+" css sliders wrapper style updated")
+    }, [state])
 
-  return (
-    <div id={"csswrapper_"+props.id} style={{ ...styleRef.current, position: 'absolute' }}>
-        <div id={"csschild_" + props.id} style={{ width: '100vw', height: '100vh', position: 'relative' }}>
-            {props.children}
-            <Dialog id={"csscontrol_" + props.id} style={{...controlStyle, position: 'absolute' }} title={'css sliders'} open={'<>'} close={'</>'}>
-                <label>{'width: '}</label>
-                {styleRef.current.width+'  '}
-                <input
-                    type="range"
-                    name={"width"}
-                    defaultValue={styleRef.current.width.split("vw")[0]}
-                    onChange={(e) => { console.log("width=" + e.target.value); styleRef.current.width = e.target.value + "vw"; setState({}); }}//(Number.parseInt(e.target.value)<85?e.target.value:"85") + "vw"; setState({}); }}
-                />
+    /*const [content, setContent] = useState(<></>)
+        useEffect(()=>{
+            setContent(<>
+                
+            </>)
+        },[])*/
 
-                <label>{'height: '}</label>
-                {styleRef.current.height+'  '}
-                <input
-                    type="range"
-                    name={"height"}
-                    defaultValue={styleRef.current.height.split("vh")[0]}
-                    onChange={(e) => { console.log("height=" + e.target.value); styleRef.current.height = e.target.value + "vh"; setState({}); }}
-                />
-
-                <label>{'x: '}</label>
-                {styleRef.current.left+'  '}
-                <input
-                    type="range"
-                    name={"left"}
-                    defaultValue={styleRef.current.left.split("vw")[0]}
-                    onChange={(e) => { console.log("x=" + e.target.value); styleRef.current.left = e.target.value + "vw"; setState({}); }}
-                />
-
-                <label>{'y: '}</label>
-                {styleRef.current.top+'  '}
-                <input
-                    type="range"
-                    name={"top"}
-                    defaultValue={styleRef.current.top.split("vh")[0]}
-                    onChange={(e) => { console.log("y=" + e.target.value); styleRef.current.top = e.target.value + "vh"; setState({}); }}
-                />
-
-                <label>{'opacity: '}</label>
-                {styleRef.current.opacity+'  '}
-                <input
-                    type="range"
-                    name={"opacity"}
-                    defaultValue={styleRef.current.opacity.split("%")[0]}
-                    onChange={(e) => { console.log("opacity=" + e.target.value); styleRef.current.opacity = e.target.value + "%"; setState({}); }}
-                />
-
-                <label>{'color: '}</label>
-                {styleRef.current.color+'  '}
-                <input
-                    type="color"
-                    name={"color"}
-                    defaultValue={styleRef.current.color}
-                    onChange={(e) => { console.log("color=" + e.target.value); styleRef.current.color = e.target.value; setState({}); }}
-                />
-
-                <label>{'background color: '}</label>
-                {styleRef.current.backgroundColor+'  '}
-                <input
-                    type="color"
-                    name={"backgroundColor"}
-                    defaultValue={styleRef.current.backgroundColor}
-                    onChange={(e) => { console.log("backgroundColor=" + e.target.value); styleRef.current.backgroundColor = e.target.value; setState({}); }}
-                />
-            </Dialog>
+    return (
+        <div id={"csswrapper_"+props.id} style={{ ...props.styleRef, ...styleRef.current, position: 'absolute' }}>
+            <div id={"csschild_" + props.id} style={{ width: '100vw', height: '100vh', position: 'relative' }}>
+                <Dialog id={"csscontrol_" + props.id} style={{...controlStyle, float: 'right' }} title={'css sliders'} open={'<>'} close={'</>'}>
+                    <Container>
+                    <Row>
+                        <Col xs={6}><label>{'width: '+styleRef.current.width+'  '}</label></Col>
+                        <Col xs={6}><input
+                            type="range"
+                            name={"width"}
+                            defaultValue={styleRef.current.width.split("vw")[0]}
+                            onChange={(e) => { console.log("width=" + e.target.value); styleRef.current.width = e.target.value + "vw"; setState({}); }}//(Number.parseInt(e.target.value)<85?e.target.value:"85") + "vw"; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'height: '+styleRef.current.height+'  '}</label></Col>
+                        <Col xs={6}><input
+                            type="range"
+                            name={"height"}
+                            defaultValue={styleRef.current.height.split("vh")[0]}
+                            onChange={(e) => { console.log("height=" + e.target.value); styleRef.current.height = e.target.value + "vh"; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'x: '+styleRef.current.left+'  '}</label></Col>
+                        <Col xs={6}><input
+                            type="range"
+                            name={"left"}
+                            defaultValue={styleRef.current.left.split("vw")[0]}
+                            onChange={(e) => { console.log("x=" + e.target.value); styleRef.current.left = e.target.value + "vw"; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'y: '}</label>
+                        {styleRef.current.top+'  '}</Col>
+                        <Col xs={6}><input
+                            type="range"
+                            name={"top"}
+                            defaultValue={styleRef.current.top.split("vh")[0]}
+                            onChange={(e) => { console.log("y=" + e.target.value); styleRef.current.top = e.target.value + "vh"; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'opacity: '}</label>
+                        {styleRef.current.opacity+'  '}</Col>
+                        <Col xs={6}><input
+                            type="range"
+                            name={"opacity"}
+                            defaultValue={styleRef.current.opacity.split("%")[0]}
+                            onChange={(e) => { console.log("opacity=" + e.target.value); styleRef.current.opacity = e.target.value + "%"; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'color: '}</label>
+                        {styleRef.current.color+'  '}</Col>
+                        <Col xs={6}><input
+                            type="color"
+                            name={"color"}
+                            defaultValue={styleRef.current.color}
+                            onChange={(e) => { console.log("color=" + e.target.value); styleRef.current.color = e.target.value; setState({}); }}
+                        /></Col>
+                    </Row>
+                    <Row>
+                        <Col xs={6}><label>{'background color: '}</label>
+                        {styleRef.current.backgroundColor+'  '}</Col>
+                        <Col xs={6}><input
+                            type="color"
+                            name={"backgroundColor"}
+                            defaultValue={styleRef.current.backgroundColor}
+                            onChange={(e) => { console.log("backgroundColor=" + e.target.value); styleRef.current.backgroundColor = e.target.value; setState({}); }}
+                        /></Col>
+                    </Row>
+                    </Container>
+                </Dialog>
+                {props.children}
+            </div>
         </div>
-    </div>
-  );
+    );
 }
--- pages/sandbox/test.tsx
+++ pages/sandbox/test.tsx
@@ -166,14 +166,52 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
 }
 
 
-
+type RowObj = {id: string, obj: string, style?: React.CSSProperties}
 export default function CSliders(props){
-    return <>
-            <CssSlidersWrapper id={'row1'}>
-                Row 1
-            </CssSlidersWrapper>
-            <CssSlidersWrapper id={'row2'}>
-                Row 2
-            </CssSlidersWrapper>
-        </>
+    const [state, setState] = useState({})
+    const [user, setUser] = useState(null)
+    const date = new Date().getTime()
+    const objects: {current: RowObj[]} = useRef([
+        {id: '0', obj: 'Row 1'},
+        {id: '1', obj: 'Row 2'}
+    ])
+    function setStyle(id: string, style: React.CSSProperties){
+        objects.current = objects.current.map((object)=>{
+            if(object.id === id){
+                object.style = {...style}
+            }
+            return object
+        })
+        setState({})
+    }
+
+    return <Container>
+            <Row>
+                <Col sm={2} md={1}>
+                    <button onClick={()=>{
+                        objects.current.push({id: (new Date().getTime()).toString(), obj: 'New Row', style: {width: '5vw', height: '5vh', left: '5vw', top: '5vh'}})
+                        setState({})
+                    }}>Add Row</button>
+                </Col>
+                <Col>
+                    <Profile ip={props.ip} setUser={setUser}/>
+                </Col>
+                <Col sm={2} md={1}>
+                    <LoginNav as={'a'} user={user} homepage={'sandbox/test'}/>
+                </Col>
+            </Row>
+            <Row style={{position: 'relative'}}>
+                {objects.current.map(
+                    (object, index)=>{
+                        return <CssSlidersWrapper key={index} style={object.style || {width: 5*(index+1)+'vw', height: 5*(index+1)+'vh', left: 5*(index+1)+'vw',top: 5*(index+1)+'vh'}} setStyle={setStyle} id={'row_'+object.id}>
+                        <a href={'#'} style={{float: "right"}} onClick={()=>{
+                            objects.current = objects.current.filter((obj)=>{return obj.id !== object.id})
+                            setState({})
+                        }}>X</a>
+                        {object.obj}
+                    </CssSlidersWrapper>
+                    }
+                )}
+            </Row>
+        </Container>
 }
