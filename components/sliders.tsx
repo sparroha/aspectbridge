02bfab27669c67d97fc6918cb4328f9b6237b1f5
Keith <940398+sparroha@users.noreply.github.com>
2023-06-10 16:50:31 -0500

    ,

--- components/sliders.tsx
+++ components/sliders.tsx
@@ -18,15 +18,18 @@ export default function CssSlidersWrapper(props) {
         opacity: '100%',
         borderRadius: '0%',
         borderWidth: '0%',
-        //fontSize: '100%',
+        fontSize: 12,
         margin: '0%',
         padding: '0%',
         color: 'black',
         display: 'flex',
-        //alignItems: 'center',
-        //justifyContent: 'center',
+        alignItems: 'stretch',
+        justifyContent: 'center',
         backgroundColor: 'gray',
+        borderStyle: 'solid',
         borderColor: 'black',
+        borderShadow: 0,
+        borderShadowColor: 'black',
         ...props.style
     })
 
@@ -48,13 +51,26 @@ export default function CssSlidersWrapper(props) {
         { name: 'left', min: 0, max: 100, unit: 'vw' },
         { name: 'top', min: 0, max: 100, unit: 'vh' },
         { name: 'opacity', min: 0, max: 100, unit: '%' },
+        { name: 'margin', min: 0, max: 100, unit: 'px' },
+        { name: 'padding', min: 0, max: 100, unit: 'px' },
+        { name: 'borderWidth', min: 0, max: 100, unit: 'px' },
         { name: 'borderRadius', min: 0, max: 50, unit: 'px' },
         { name: 'borderRadius', min: 0, max: 50, unit: '%' },
+        { name: 'borderShadow', min: 0, max: 50},
+        { name: 'fontSize', min: 0, max: 100},
+    ]
+    const slidersOption = [
+        { name: 'display', options: ['flex', 'block', 'inline-block', 'inline', 'none'] },
+        { name: 'alignItems', options: ['center', 'flex-start', 'flex-end', 'baseline', 'stretch'] },
+        { name: 'justifyContent', options: ['center', 'flex-start', 'flex-end', 'space-between', 'space-around', 'space-evenly'] },
+        { nane: 'borderStyle', options: ['solid', 'dotted', 'dashed', 'double', 'groove', 'ridge', 'inset', 'outset', 'none', 'hidden'] },
+        { name: 'position', options: ['absolute', 'relative', 'fixed', 'sticky']}
     ]
     const slidersColor = [
         { name: 'color' },
         { name: 'backgroundColor' },
         { name: 'borderColor' },
+        { name: 'borderShadowColor' },
     ]
     const q = false
     if(q)return (
@@ -95,7 +111,7 @@ export default function CssSlidersWrapper(props) {
         </div>
     )
     else return <>
-        <div id={"csswrapper_"+props.id} style={{ ...props.styleRef, ...styleRef.current, position: 'absolute' }}>
+        <div id={"csswrapper_"+props.id} style={{...props.styleRef, ...styleRef.current, position: 'absolute' }}>
             <div id={"csschild_" + props.id} style={{ width: '100vw', height: '100vh', position: 'relative' }}>
                 <div id={"csscontrolerbutton_" + props.id} className={style.controlStyle} title={'css sliders'}>
                     <Button id={'csscontroleropen_'+props.id} onClick={
@@ -105,13 +121,13 @@ export default function CssSlidersWrapper(props) {
                 {props.children}
             </div>
         </div>
-        {controlerOpen?<CssControler sliders={sliders} slidersColor={slidersColor} styleRef={styleRef} setState={setState}/>:null}
+        {controlerOpen?<CssControler sliders={sliders} slidersOption={slidersOption} slidersColor={slidersColor} styleRef={styleRef} setState={setState}/>:null}
     </>
 
 }
 
 export function CssControler(props){
-    const {sliders, slidersColor, styleRef, setState} = props
+    const {sliders, slidersOption, slidersColor, styleRef, setState} = props
     return <div id={"csscontroler_" + props.id} style={{position: 'absolute', right: '0%', top: '0%', width: '300px', borderRadius: '25px'}}>
         <div id={"csscontrolerBackground_" + props.id} className={'grey-back o4 w100 h100'} style={{position: 'absolute', borderRadius: '25px'}}></div>{/**translucent backdrop */}
         <div id={"csscontroler_" + props.id} style={{position: 'relative'}}>
@@ -127,6 +143,19 @@ export function CssControler(props){
                         defaultValue={slider.unit?styleRef.current[slider.name].split(slider.unit)[0]:styleRef.current[slider.name]}
                         onChange={(e) => { console.log(slider.name + "=" + e.target.value); styleRef.current[slider.name] = slider.unit?(e.target.value + slider.unit):e.target.value; setState({}); }}
                     /></Col>
+                </Row>
+            })}
+            {slidersOption.map((slider, i) => {
+                return <Row key={i} style={{zIndex: 2}}>
+                    <Col xs={6}><label>{slider.name + ': '+styleRef.current[slider.name]+'  '}</label>
+                    </Col>
+                    <Col xs={6}><select
+                        name={slider.name}
+                        defaultValue={styleRef.current[slider.name]}
+                        onChange={(e) => { console.log(slider.name + "=" + e.target.value); styleRef.current[slider.name] = e.target.value; setState({}); }}
+                    >{slider.options.map((option, i) => {
+                        return <option key={i} value={option}>{option}</option>
+                    })}</select></Col>
                 </Row>
             })}
             {slidersColor.map((slider, i) => {
