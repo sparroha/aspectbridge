e2b91cb95e5736037e3bb3b840265f7ef1bebd4d
Keith <940398+sparroha@users.noreply.github.com>
2023-06-14 17:38:11 -0500

    .

--- pages/sandbox/registrytest.tsx
+++ pages/sandbox/registrytest.tsx
@@ -2,11 +2,13 @@ import useRegister from "../../lib/util/registry"
 
 export default function Registry(props){
     const [test, setTest] = useRegister('test', 'off')
+    const [testobject, setTestobject] = useRegister('testobject', {toggle: 'off'})
 
     return <>
         <div id={'test'}>{test}</div>
         <button onClick={()=>{setTest(test=='off'?'on':'off')}}>Toggle</button>
 
+        <div id={'testobject'}>{testobject.toggle}</div>
+        <button onClick={()=>{setTestobject({toggle: testobject.toggle=='off'?'on':'off'})}}>Toggle</button>
     </>
-
 }
