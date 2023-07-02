6a3e3cdd36841a2d1b930f9735deab882e8e891a
Keith <anthymngalaris@gmail.com>
2023-07-01 22:49:35 -0400

    .

--- pages/sandbox/story2.tsx
+++ pages/sandbox/story2.tsx
@@ -240,7 +240,7 @@ export default function Story(props) {
 		if(!activeUsers)return
 		const L = (e)=>{
 			console.log('why?')
-			setDB(ACTIVEUSERS,[...JSON.parse(activeUsers).filter((user)=>{return user.name!=user.username}), {name: user.username, time: new Date().getTime()}])
+			setDB(ACTIVEUSERS,[...(JSON.parse(activeUsers).filter((user)=>{return user.name!=user.username})), {name: user.username, time: new Date().getTime()}])
 		}
 		document.addEventListener('click', L)
 		return ()=>document.removeEventListener('click',L)
