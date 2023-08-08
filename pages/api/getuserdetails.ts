7a9d3dc0b200c601007b06b98bf76e953e360095
Keith <940398+sparroha@users.noreply.github.com>
2023-08-07 19:40:42 -0500

    .

--- pages/api/getuserdetails.ts
+++ pages/api/getuserdetails.ts
@@ -9,6 +9,7 @@ export default async function getUserDetails(req, res): Promise<Partial<User>> {
     if(command) switch(command){
         case 'getall':
             user = await getAllUsers()
+            if(!user) res.status(400).json({message: 'No users found.'})
             res.status(200).json(user);
             break;
         case 'delete':
