e8c805cd62913d5d74d2b02e0b961a54ed683932
Keith <940398+sparroha@users.noreply.github.com>
2023-08-19 18:44:27 -0500

    .

--- pages/chat/index.tsx
+++ pages/chat/index.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useState, useEffect } from 'react';
 import { ACTIVEUSERS, ActiveUser, LoginNav, Profile } from '../login/[userlogin]';
 import { GetServerSideProps } from 'next';
