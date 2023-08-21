5ff9b540d3c0777c69b8176a52831d67f4358682
Keith <940398+sparroha@users.noreply.github.com>
2023-08-20 21:06:23 -0500

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,6 @@ export const metadata: Metadata = {
 }
 export default async function RootLayout({children}: {children: React.ReactNode}) {
   return <html lang={'en'}>
-    <header><h1>DEV</h1></header>
     <body>{children}</body>
   </html>
 }
