b1f6b1f5664235501cb5fc210177bf4ece52fd98
Keith <940398+sparroha@users.noreply.github.com>
2023-08-21 08:53:34 -0500

    .

--- app/bridge/[...aspect]/layout.tsx
+++ app/bridge/[...aspect]/layout.tsx
@@ -1,23 +1,8 @@
 'use client'
-import { Metadata } from 'next'
+
 import NavLeftDefault from '../../navleft'
 import NavRightDefault from '../../navright'
 
-export async function generateMetadata({ params }): Promise<Metadata> {
-  // read route params
-  const aspect: string[] = params.aspect
- 
-  // fetch data
-  //const product = await fetch(`https://.../${id}`).then((res) => res.json())
- 
-  // optionally access and extend (rather than replace) parent metadata
-  const arkTitle = ''//(await parent).title
- 
-  return {
-    title: arkTitle+'Aspect Bridge Home',
-    icons: '/public/assets/binary2.png',
-  }
-}
 export default async function BridgeLayout({children}) {
   return <div className='row'>
       <NavLeftDefault/>
--- app/bridge/[...aspect]/page.tsx
+++ app/bridge/[...aspect]/page.tsx
@@ -55,7 +55,7 @@ export async function generateMetadata({ params }: pageProps, parent: ResolvingM
       title: arkTitle+' '+aspect[0],
       icons: '/public/assets/binary2.png',
     }
-  }
+}
 /**
  * This is the Primary function of the web site. All dunamic rendering is processed here
  * 
