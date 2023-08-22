fb020f0c33e9b4c161204c729d526987afeec5d4
Keith <940398+sparroha@users.noreply.github.com>
2023-08-22 00:13:42 -0500

    useHashCookie

--- lib/util/^hashcookie.ts
+++ lib/util/^hashcookie.ts
@@ -1,8 +1,8 @@
 'use client'
 import { Dispatch, SetStateAction, useEffect, useMemo, useState } from "react"
 
-export function useHashCookie(): [string, Dispatch<SetStateAction<string>>]{
-    const [hash, setHash] = useState(null)
+export function useHashCookie(searchParams?): [string, Dispatch<SetStateAction<string>>]{
+    const [hash, setHash] = useState(searchParams?.hash || null)
     const [cookie, setCookie] = useState(null)
 
     /**
@@ -10,24 +10,30 @@ export function useHashCookie(): [string, Dispatch<SetStateAction<string>>]{
      */
     //get local cookie
     useEffect(()=>{
+        if(!document.cookie)console.log('@^HashCookie: no cookie ', document.cookie)
         if(!cookie)setCookie(document.cookie)
+        else console.log('@^HashCookie: got cookie ', cookie)
         console.log('@^HashCookie: getting cookie ', document.cookie)
     },[cookie])
     //get parse array from cookie
-    const parsedCookies: [[string, string]] = useMemo(()=>{
+    const parsedCookies: [[string, string]] | null = useMemo(()=>{
+        if(!cookie) return null
         console.log('@^HashCookie: parsing cookie ', cookie)
-        return cookie?cookie.split(';').map((c)=>c.trim().split('=')):null
+        return cookie.split(';').map((c)=>c.trim().split('='))
     },[cookie])
     //get secret hash from parsed array
-    const secretHash: string = useMemo(()=>{
+    const secretHash: string | null = useMemo(()=>{
+        if(!parsedCookies) return null
         console.log('@^HashCookie: extracting secret from parsed', parsedCookies)
-        let secret: [string, string] = parsedCookies?parsedCookies.find((c)=>c[0]=='secret'):null
+        let [,secret] = parsedCookies.find((c, i)=>c[0]=='secret')
+        if(!secret) return null
         console.log('@^HashCookie: extracting secret value from pair', secret)
-        return secret?.[1]?secret[1]:null
+        return secret
     },[parsedCookies])
     //set hash from cookie secret
     useEffect(()=>{
-        if(secretHash)setHash(secretHash)
+        if(!secretHash) return
+        setHash(secretHash)
         console.log('@^HashCookie: sending secret to client page ', secretHash)
     },[secretHash])
     /**
@@ -38,12 +44,13 @@ export function useHashCookie(): [string, Dispatch<SetStateAction<string>>]{
      * IF VALID HASH
      */
     //new cookie only valid hash from logged in user
-    const newcookie = useMemo(()=>hash?`secret=${hash}; max-age=604800; domain=aspectbridge.com`:null,[hash])
+    const newcookie: string | null = useMemo(()=>hash?`secret=${hash}; max-age=604800; path=/`:null,[hash])
     //save cookie once user is logged in
     useEffect(()=>{
-        console.log('@^HashCookie: sending new cookie to local save ', newcookie)
         if(!newcookie)return
+        console.log('@^HashCookie: sending new cookie to local save ', newcookie)
         document.cookie = newcookie
+        console.log('@^HashCookie: now cookie', document.cookie)
     },[newcookie])
     /**
      * THEN SET COOKIE
