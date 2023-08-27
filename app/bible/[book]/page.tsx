e881ca3371d84df43659af951ce02069d7dd48df
Keith <940398+sparroha@users.noreply.github.com>
2023-08-27 04:58:44 -0500

    .

--- app/bible/[book]/page.tsx
+++ app/bible/[book]/page.tsx
@@ -2,23 +2,22 @@ import { useRouter } from "next/navigation"
 
 export default function Book(props){
     const router = useRouter()
-    const title = props.params.book[0]
+    const title = props?.params?.book[0] || ''
     
     return <>
         {title}
-        {//<Chapter {...props}/>
-}
+        <Chapter {...props}/>
     </>
 }
 export function Chapter(props){
-    const title = props.params.book[1]
+    const title = props?.params?.book[1] || ''
     return <>
         {title}
         <Verse {...props}/>
     </>
 }
 export function Verse(props){
-    const title = props.params.book[2]
+    const title = props?.params?.book[2] || ''
     return <>
     </>
 }
