929fcb881e51041eb149265fde43de284f3d956d
Keith <940398+sparroha@users.noreply.github.com>
2023-09-05 06:16:14 -0500

    .

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export default function Index(p){
     return <>
-        <Link href="/projects/game-dev-on-the-go">Game Dev on the Go</Link>
-        <Link href="/projects/chess">Chess</Link>
-        <Link href="/projects/cost">Cost Idle Clicker</Link>
-        <Link href="/projects/canvas">Canvas</Link>
-        <Link href="/projects/story">Story Board</Link>
+        <Link href="/projects/game-dev-on-the-go">Game Dev on the Go</Link><br/>
+        <Link href="/projects/chess">Chess</Link><br/>
+        <Link href="/projects/cost">Cost Idle Clicker</Link><br/>
+        <Link href="/projects/canvas">Canvas</Link><br/>
+        <Link href="/projects/story">Story Board</Link><br/>
     </>
 }
