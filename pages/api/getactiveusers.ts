2529084e19b716914e565aa034fcc6422af02941
Keith <940398+sparroha@users.noreply.github.com>
2023-09-05 14:03:35 -0500

    .

--- pages/api/getactiveusers.ts
+++ pages/api/getactiveusers.ts
@@ -1,5 +1,6 @@
+import { ActiveUser } from '../../app/api/users/active/route';
 import sql from '../../lib/,base/sql';
-import { ACTIVEUSERS, ActiveUser } from '../../lib/util/^activeusers';
+import { ACTIVEUSERS } from '../../lib/util/^activeusers';
 import { RegistryEntry } from './registry_old/[registry]';
 export default async function getActiveUsers(req, res){
     const nonuser: ActiveUser = {name: '420: No Users Active', access: 2, time: Date.now()}
