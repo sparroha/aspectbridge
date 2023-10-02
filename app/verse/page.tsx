8d75415020f7a428a1c1e2106529201a11b202c3
Keith <940398+sparroha@users.noreply.github.com>
2023-10-02 07:12:58 -0500

    .

--- app/verse/page.tsx
+++ app/verse/page.tsx
@@ -1,19 +1,19 @@
 'use client'
 import { useReducer, useState, useEffect, createContext, useContext } from 'react'
 import { Button, Col, Form, Row } from "react-bootstrap";
-import useUser from '../../../lib/util/^user';
+import useUser from '../../lib/util/^user';
 import Link from 'next/link';
-import { BuildRaft, BuildShip, ChiselSlabs, Choice, ChopWood, DigClay, FireBricks, Fish, InfoHeader, MineOre, Portal, QuaryStone, Sail, SawLumber, ScribeTablet, ScriptDirectory, SmeltMetal, Zone } from './zones';
-import { initialState, useZRContext } from './provider';
-import { useUserSave } from '../../../lib/util/^userSave';
+import { BuildRaft, BuildShip, ChiselSlabs, Choice_experimental as Choice, ChopWood, DigClay, FireBricks, Fish, InfoHeader, MineOre, Portal, QuaryStone, Sail, SawLumber, ScribeTablet, ScriptDirectory, SmeltMetal, FormGroup, ContentPanel, InfoPanel, ChatPanel, InterfacePanel, DisplayGroup } from './forms';
+import { initialState, useVerseContext } from './provider';
+import { useUserSave } from '../../lib/util/^userSave';
 //----------------------------------------------------------------------------------------------------------------------------------
 
 //----------------------------------------------------------------------------------------------------------------------------------
 export default function Go(p){
     /**CONSTANTS**/
-    const {state, dispatch} = useZRContext()//138 lines
+    const {state, dispatch} = useVerseContext()//138 lines
     const user = useUser()//22
-    const [saveLoad, loadSave, userLoaded] = useUserSave('on_the_go', user?.username, state, dispatch)//40 lines
+    const [saveLoad, loadSave, userLoaded] = useUserSave('verse', user?.username, state, dispatch)//40 lines
     
     const [autoSaveInterval, setAutoSaveInterval] = useState(10)
     //----------------------------------------------------------------------------------------------------------------------------------
@@ -79,7 +79,7 @@ export default function Go(p){
             <Col xs={12} sm={6} md={3} lg={2}><Link href={'https://www.materiamagica.com'}>Materia Magica</Link></Col>
             <Col xs={12} sm={6} md={3} lg={2}><Link href={'/registry/on_the_go:'+user?.username+'?id=true&name=true&data=true'}>{user?.username}'s raw data'</Link></Col>
         </InfoHeader></Row>
-        <Row><InfoHeader id="inventory_resources" bgColor={'#aaa'}>
+        {/*<Row><InfoHeader id="inventory_resources" bgColor={'#aaa'}>
             {['Resources', 'stone', 'wood', 'ore', 'clay', 'fish'].map((item, i)=>{
                 return <Col key={i} xs={4} sm={3} md={2} lg={1} style={{textAlign: 'center'}}>
                     {(item=='Resources' || item=='Materials')?item:
@@ -94,50 +94,63 @@ export default function Go(p){
                             <>{item}<br/>{state[item.toLowerCase()]}</>}
                 </Col>
             })} 
-        </InfoHeader></Row>
+        </InfoHeader></Row>*/}
         {/**
          * All valid forms must be registered here in order to render to app page
-         * This includes all Zone components and their children
+         * This includes all FormGroup components and their children
          */}
-        <Row id="interface-zones">
-            <Zone id={"Mine"} bgColor={"#aa7"} bgAlt={'Gold Mine'} helper bgImage={'https://www.automation.com/getmedia/f4d4cca4-3167-4426-803a-de780ccefab9/Gold-mine-feature-July-29-2021-web.png?width=500&height=313&ext=.png'}>
+        <Row id="display-group">
+            <DisplayGroup id={"Display"} bgColor={"#aa7"} bgImage={''}>
+                <ContentPanel/>
+                <InfoPanel/>
+            </DisplayGroup>
+        </Row>
+        <Row id="control-group">
+            <FormGroup id={"Control"} bgColor={"#aa7"} bgAlt={''} bgImage={''}>
+                <ChatPanel/>
+                <InterfacePanel/>
+                <InfoPanel/>
+            </FormGroup>
+        </Row>
+        <Row id="form-groups">
+            <FormGroup id={"Aurical"} bgGradient={'radial-gradient(#fff, #fff 50%, #90a0f088 52%, #90a0f088 60%, #00000000 70%)'} bgAlt={'NGC 1433'}>
+                <Choice/>
+            </FormGroup>
+            <FormGroup id={"Mine"} bgColor={"#aa7"} bgAlt={'Gold Mine'} helper bgImage={'https://www.automation.com/getmedia/f4d4cca4-3167-4426-803a-de780ccefab9/Gold-mine-feature-July-29-2021-web.png?width=500&height=313&ext=.png'}>
                 <MineOre/>
                 <SmeltMetal/>
-            </Zone>
-            <Zone id={"Quary"} bgColor={"grey"} bgAlt={'Colonial Marble & Granite'} helper bgImage={'https://www.colonialmarble.net/wp-content/uploads/2021/08/shutterstock_523267222.jpg'}>
+            </FormGroup>
+            <FormGroup id={"Quary"} bgColor={"grey"} bgAlt={'Colonial Marble & Granite'} helper bgImage={'https://www.colonialmarble.net/wp-content/uploads/2021/08/shutterstock_523267222.jpg'}>
                 <QuaryStone/>
                 <ChiselSlabs/>
-            </Zone>
-            <Zone id={"Forest"} bgColor={"green"} bgAlt={'Deposit Photos copyright'} helper bgImage={'https://st4.depositphotos.com/11328482/31492/i/450/depositphotos_314925876-stock-photo-summer-green-fir-forest-landscape.jpg'}>
+            </FormGroup>
+            <FormGroup id={"Forest"} bgColor={"green"} bgAlt={'Deposit Photos copyright'} helper bgImage={'https://st4.depositphotos.com/11328482/31492/i/450/depositphotos_314925876-stock-photo-summer-green-fir-forest-landscape.jpg'}>
                 <ChopWood/>
                 <SawLumber/>
                 <BuildRaft/>
-            </Zone>
-            <Zone id={"River"} bgColor={"blue"} bgAlt={'River'} helper bgImage={'https://static.toiimg.com/photo/msid-101267463,width-96,height-65.cms'}>
+            </FormGroup>
+            <FormGroup id={"River"} bgColor={"blue"} bgAlt={'River'} helper bgImage={'https://static.toiimg.com/photo/msid-101267463,width-96,height-65.cms'}>
                 <Sail/>
                 <Fish/>
                 <DigClay/>
                 <FireBricks/>
-            </Zone>
-            <Zone id={"Port"} bgColor={"blue"} bgAlt={'Harbor'} helper bgImage={'https://i.ytimg.com/vi/ZznX01ViFTc/maxresdefault.jpg'}>
+            </FormGroup>
+            <FormGroup id={"Port"} bgColor={"blue"} bgAlt={'Harbor'} helper bgImage={'https://i.ytimg.com/vi/ZznX01ViFTc/maxresdefault.jpg'}>
                 <Sail/>
                 <Fish/>
-            </Zone>
-            <Zone id={"Shipyard"} bgColor={"blue"} bgAlt={'Shipyard'} bgImage={'https://thebridgebk.com/wp-content/uploads/2018/02/20170512-Z74A1290-2-e1519352429367.jpg'}>
+            </FormGroup>
+            <FormGroup id={"Shipyard"} bgColor={"blue"} bgAlt={'Shipyard'} bgImage={'https://thebridgebk.com/wp-content/uploads/2018/02/20170512-Z74A1290-2-e1519352429367.jpg'}>
                 <BuildShip/>
-            </Zone>
-            <Zone id={"Library"} bgColor={"#79f"} bgAlt={'Library'} helper bgImage={'https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg?auto=compress&cs=tinysrgb&w=700'}>
+            </FormGroup>
+            <FormGroup id={"Library"} bgColor={"#79f"} bgAlt={'Library'} helper bgImage={'https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg?auto=compress&cs=tinysrgb&w=700'}>
                 <ScribeTablet/>
-            </Zone>
-            <Zone id={"Scripts"} bgColor={"#79f"} bgAlt={'Library'} bgImage={'https://i.insider.com/5720d775dd0895167d8b468b?width=700'}>
+            </FormGroup>
+            <FormGroup id={"Scripts"} bgColor={"#79f"} bgAlt={'Library'} bgImage={'https://i.insider.com/5720d775dd0895167d8b468b?width=700'}>
                 <ScriptDirectory/>
-            </Zone>
-            <Zone id={"Tower"} bgColor={"#79f"} bgAlt={'NGC 1433'} bgImage={'https://stsci-opo.org/STScI-01GS6A1YR1W0CXGTPG0VX1FTZA.png'}>
+            </FormGroup>
+            <FormGroup id={"Tower"} bgColor={"#79f"} bgAlt={'NGC 1433'} bgImage={'https://stsci-opo.org/STScI-01GS6A1YR1W0CXGTPG0VX1FTZA.png'}>
                 <Portal/>
-            </Zone>
-            <Zone id={"Aurical"} bgGradient={'radial-gradient(#fff, #fff 50%, #90a0f088 52%, #90a0f088 60%, #00000000 70%)'} bgAlt={'NGC 1433'}>
-                <Choice/>
-            </Zone>
+            </FormGroup>
         </Row>
     </div>
 }
---
+++ app/verse/provider.tsx
@@ -0,0 +1,141 @@
+'use client'
+import { createContext, useContext, useReducer } from "react"
+
+type Location = 'Mine' | 'Quary' | 'Forest' | 'River' | 'Port' | 'Shipyard' | 'Library' | 'Scripts' | 'Tower' | 'Aurical' |
+    'ContentPanel' | 'InfoPanel' | 'ChatPanel' | 'InterfacePanel'
+type LocationInfo = {name: string, forms: Location[]}
+
+/**
+ * each location represents a page content and each zone represents a form interface on that page
+ * interfaces will not render if not included in this zone list unless they are standalone and do not use the zone constraints
+ * 
+ * const {state, dispatch} = useZRContext()
+ * if(!state.location?.zones?.includes(id))return
+ * 
+ */
+export const formgroups: {[key: string]: LocationInfo} = {
+    island: {name: 'Island', forms: ['Mine', 'Quary', 'Forest', 'River', 'Library', 'Tower']},
+    mainland: {name: 'Mainland', forms: ['Quary', 'Forest', 'Port', 'Shipyard', 'Library', 'Scripts']},
+    styx: {name: 'Styx', forms: ['Aurical', 'Forest', 'River'/*, 'Scripts'*/]},
+    display: {name: 'Display', forms: ['ContentPanel','InfoPanel']},
+    control: {name: 'Control', forms: ['ChatPanel','InterfacePanel','InfoPanel']}
+}
+
+export const initialState = {
+    location: formgroups.styx
+}
+
+const VerseContext = createContext(null)
+
+export const useVerseContext = ()=>{
+    const context = useContext(VerseContext)
+    if(!context) throw new Error(
+        "VerseContext not available in scope"
+    )
+    return context
+}
+
+export default function VerseContextProvider({children}){
+    /**
+     * These are the rules for all manipulations handled by the user intertface
+     * @param state 
+     * @param action 
+     * @returns 
+     */
+    const reducer = (state: any, action: {type: string, payload?: any})=>{
+        let actionType = action.type.toLowerCase()
+        switch(actionType){
+            case 'teleport':
+                return window.location.href = '/'+action.payload.destination
+            case 'set':
+                return action.payload != "default" ? action.payload : initialState
+            case 'loop':
+                console.log('loop attempt')
+                let roll = Math.random()*10+1 //d10
+                let success: boolean = (roll<=3) //30% chance
+                if(!success)return state
+                console.log('loop success')
+                return {...state,
+                    //(state.helpers && state.helpers[type])
+                    ore: Math.floor(Math.random()*(state.helpers?.mine?state.helpers.mine+1:1)) + (state.ore?state.ore:0),
+                    stone: Math.floor(Math.random()*(state.helpers?.quary?state.helpers.quary+1:1)) + (state.stone?state.stone:0),
+                    wood: Math.floor(Math.random()*(state.helpers?.forest?state.helpers.forest+1:1)) + (state.wood?state.wood:0),
+                    fish: Math.floor(Math.floor(Math.random()*(state.helpers?.river?state.helpers.river+1:1))/2) + Math.floor(Math.random()*(state.helpers?.port?state.helpers.port+1:1)) + (state.fish?state.fish:0),
+                    clay: Math.floor(Math.floor(Math.random()*(state.helpers?.river?state.helpers.river+1:1))/2) + Math.floor(Math.random()*(state.helpers?.library?state.helpers.library+1:1)) + (state.clay?state.clay:0)
+                }
+            case 'add'://{type: 'ore', count: 1}
+                return {...state, [action.payload.type]: state[action.payload.type]?state[action.payload.type]+action.payload.count:action.payload.count}
+            case 'remove'://{type: 'wood', count: 1}
+                if(!state[action.payload.type]) {alert("strange; it seems you haven't discovered that yet"); return state}
+                if(state[action.payload.type]<action.payload.count) {alert('not enough '+action.payload.type); return state}
+                return {...state, [action.payload.type]: state[action.payload.type]-action.payload.count}
+            case 'exchange'://{trade: 'ore', amount: 1, type: 'metal', count: 1}
+                if(!state[action.payload.trade]) {alert("strange; it seems you haven't discovered that yet"); return state}
+                if(!state[action.payload.type]) {alert("strange; it seems you haven't discovered that yet"); return state}
+                if(state[action.payload.trade]<action.payload.amount) {alert('not enough '+action.payload.trade); return state}
+                return {...state, [action.payload.trade]: state[action.payload.trade]-action.payload.amount, [action.payload.type]: state[action.payload.type]?state[action.payload.type]+action.payload.count:action.payload.count}
+            case 'craft':
+                let craftType = action.payload.type.toLowerCase()
+                switch(craftType){
+                    case 'metal':
+                        if(state.ore<3) {alert('not enough ore'); return state}
+                        if(state.wood<3) {alert('not enough wood'); return state}
+                        return {...state, ore: state.ore-3, wood: state.wood-3, metal: state.metal?state.metal+2:2}
+                    case 'brick':
+                        if(state.clay<4) {alert('not enough clay'); return state}
+                        if(state.wood<3) {alert('not enough wood'); return state}
+                        return {...state, clay: state.clay-4, wood: state.wood-3, brick: state.brick?state.brick+2:2}
+                    case 'lumber':
+                        if(state.wood<1) {alert('not enough wood'); return state}
+                        return {...state, wood: state.wood-1, lumber: state.lumber?state.lumber+3:3}
+                    case 'tile':
+                        if(state.stone<1) {alert('not enough stone'); return state}
+                        if(state.lumber<4) {alert('not enough lumber'); return state}
+                        return {...state, stone: state.stone-1, lumber: state.lumber-4, tile: state.tile?state.tile+4:4}
+                    case 'boat':
+                        if(state.lumber<30) {alert('not enough lumber'); return state}
+                        return {...state, lumber: state.lumber-30, boat: state.boat?state.boat+1:1}
+                    case 'ship':
+                        if(state.lumber<40) {alert('not enough lumber'); return state}
+                        if(state.brick<40) {alert('not enough brick'); return state}
+                        if(state.metal<20) {alert('not enough metal'); return state}
+                        return {...state, lumber: state.lumber-40, brick: state.brick-40, metal: state.metal-20, ship: state.ship?state.ship+1:1}
+                    case 'flagship':
+                        if(state.lumber<80) {alert('not enough lumber'); return state}
+                        if(state.wood<40) {alert('not enough wood'); return state}
+                        if(state.brick<80) {alert('not enough bick'); return state}
+                        if(state.metal<60) {alert('not enough metal'); return state}
+                        return {...state, lumber: state.lumber-80, wood: state.wood-40, brick: state.brick-80, metal: state.metal-60, flagship: state.flagship?state.flagship+1:1}
+                    case 'raft':
+                        if(state.wood<20) {alert('not enough wood'); return state}
+                        return {...state, wood: state.wood-20, raft: state.raft?state.raft+1:1}
+                }
+            case 'write':
+                if(state.brick<1 || state.lumber<1) {alert('not enough bricks or lumber'); return state}
+                return {...state, brick: state.brick-1, lumber: state.lumber-1, tablets: state.tablets?[...state.tablets, action.payload.message]:[action.payload.message]}
+            case 'sail'://{destination: 'Island', vessel: 'boat'}
+                if(state.fish<20) {alert('not enough fish'); return state}
+                let destination = action.payload.destination.toLowerCase()
+                if(state.location.name==destination) {alert('this is your current location'); return state}
+                let vessel = action.payload.vessel.toLowerCase()
+                if(!state[vessel]) {alert('you seem to have not found one of those '+vessel+'s yet'); return state}
+                if(state[vessel]<1) {alert('no '+vessel+'s available'); return state}
+                let durability = (vessel=='boat'?4:(vessel=='ship'?10:(vessel=='flagship'?20:2)))
+                let loss = Math.floor(Math.random()*durability)<1?1:0
+                if(loss>=1) {alert('Your '+vessel+' was lost at sea!'); return {...state, location: formgroups[destination], [vessel]: state[vessel]-1, fish: state.fish-20}}
+                return {...state, location: formgroups[destination], fish: state.fish-20}
+            case 'hire'://{type: 'Mine', fee: 100}
+                if(state.fish<(action.payload.fee || 200)) {alert('not enough fish'); return state}
+                let type = action.payload.type.toLowerCase()
+                return {...state, fish: state.fish-(action.payload.fee || 200), helpers: {...state.helpers, [type]: (state.helpers && state.helpers[type])?state.helpers[type]+1:1}}
+            default:
+                return state
+        }
+    }
+    const [state, dispatch] = useReducer(reducer, initialState)
+    return <VerseContext.Provider
+        value={{state, dispatch}}
+    >
+        {children}
+    </VerseContext.Provider>
+}
