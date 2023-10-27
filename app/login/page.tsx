4eaa3971a3915a2e63e205e2d108841d6563f7a5
Keith <940398+sparroha@users.noreply.github.com>
2023-10-27 11:09:54 -0500

    .

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,6 @@ import { useRouter } from "next/navigation"
 
 export default function Login({params}){
     const r = useRouter()
-    r.push('/login/login')
+    r.push('login/login')
     return <>Login...</>
 }
