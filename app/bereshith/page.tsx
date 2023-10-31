b2443ff5e760850178030e258860ab49a3d02f88
Keith <940398+sparroha@users.noreply.github.com>
2023-10-31 14:05:17 -0500

    .

--- app/bereshith/page.tsx
+++ app/bereshith/page.tsx
@@ -77,6 +77,9 @@ export default function Beginning({params, searchParams}){
             mah: 'what?',
             mi: 'who',
         },
+        wet: {
+
+        },
         eretz: {
             titles: ['earth'/* what exists in earth with Him */],
             a: 'one',
