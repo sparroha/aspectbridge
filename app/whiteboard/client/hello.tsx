ca7232484678cd81dc54d603a05f08c44528e43d
Keith <940398+sparroha@users.noreply.github.com>
2024-01-20 17:50:23 -0500

    .

--- app/whiteboard/client/hello.tsx
+++ app/whiteboard/client/hello.tsx
@@ -2,7 +2,32 @@
 
 import { stylex } from "@stylexjs/stylex"
 import { useEffect } from "react"
-import { styles } from "../layout"
+
+const styles = stylex.create({
+    root: {
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+    },
+    title: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+    },
+    button: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 10,
+        borderRadius: 5,
+        backgroundColor: '#eee',
+        cursor: 'pointer',
+        ':hover': {
+            backgroundColor: '#ddd',
+        },
+    },
+})
 
 export default function Hello(){
     useEffect(()=>{
--- app/whiteboard/layout.tsx
+++ app/whiteboard/layout.tsx
@@ -1,31 +1,4 @@
 import { neonLaser } from "../../lib/util/gfx/laser";
-import stylex from "@stylexjs/stylex"
-
-const styles = stylex.create({
-    root: {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        height: '100%',
-    },
-    title: {
-        fontSize: 24,
-        fontWeight: 'bold',
-        marginBottom: 20,
-    },
-    button: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        padding: 10,
-        borderRadius: 5,
-        backgroundColor: '#eee',
-        cursor: 'pointer',
-        ':hover': {
-            backgroundColor: '#ddd',
-        },
-    },
-})
 
 export default async function RootLayout({children}: {children: React.ReactNode}) {
     return <div id="content" className='row' style={{height: '100%'}}>
