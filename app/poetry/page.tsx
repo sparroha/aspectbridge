2c05fa47a8c833dfb227888d166b382b6ed5171a
Keith <940398+sparroha@users.noreply.github.com>
2024-01-20 17:33:23 -0500

    ,
    ,

--- app/poetry/page.tsx
+++ app/poetry/page.tsx
@@ -11,26 +11,26 @@ export default function Page({params, searchParams}){
     const {state, dispatch} = useDynamicContext()
 
     return <div id={'field'} style={{position: 'relative', background: 'white', height: '100%'}}>
-        <table>
+        <table><tbody>
             <tr style={{borderBottom: '1px solid black'}}> <th>Yimir</th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th> </tr>
             <tr> <td>Not God</td>&nbsp;|&nbsp;<td>Father to 3 or 4 races</td>&nbsp;|&nbsp;<td>first man</td>&nbsp;|&nbsp;<td>First Rime-giant</td> </tr>
-        </table>
-        <table>
+        </tbody></table>
+        <table><tbody>
             <tr style={{borderBottom: '1px solid black'}}> <th>Yimir</th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th> </tr>
             <tr> <td>Not God</td>&nbsp;|&nbsp;<td>Father to 3 or 4 races</td>&nbsp;|&nbsp;<td>first man</td>&nbsp;|&nbsp;<td>First Rime-giant</td> </tr>
-        </table>
-        <table>
+        </tbody></table>
+        <table><tbody>
             <tr style={{borderBottom: '1px solid black'}}> <th>Yimir</th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th> </tr>
             <tr> <td>Not God</td>&nbsp;|&nbsp;<td>Father to 3 or 4 races</td>&nbsp;|&nbsp;<td>first man</td>&nbsp;|&nbsp;<td>First Rime-giant</td> </tr>
-        </table>
-        <table>
+        </tbody></table>
+        <table><tbody>
             <tr style={{borderBottom: '1px solid black'}}> <th>Yimir</th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th>&nbsp;&nbsp;<th></th> </tr>
             <tr> <td>Not God</td>&nbsp;|&nbsp;<td>Father to 3 or 4 races</td>&nbsp;|&nbsp;<td>first man</td>&nbsp;|&nbsp;<td>First Rime-giant</td> </tr>
-        </table>
+        </tbody></table>
     </div>
 }
 
-export function SelectableItem({children}){
+export const SelectableItem = ({children})=>{
     return <div>
         {children}
     </div>
