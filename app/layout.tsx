66ad0bc04ce0d08773e9460ffb081d9e24dc2eab
Keith <940398+sparroha@users.noreply.github.com>
2024-07-07 05:11:29 -0400

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import '../public/css/helper.css'
 import { Metadata, Viewport } from 'next'
 import LayoutHeader from './layout_header'
 
+export const galaxyImg = 'https://stsci-opo.org/STScI-01GS6A1YR1W0CXGTPG0VX1FTZA.png'
 export const metadata: Metadata = {
   title: 'Aspect Bridge Root',
   description: 'Aspect Bridge Test Platform',
@@ -13,7 +14,7 @@ export const metadata: Metadata = {
   keywords: 'Aspect Bridge, Aspect, Bridge, AspectBridge, Aspect-Bridge, Aspect_Bridge, AspectBridge.com, Aspect-Bridge.com, Aspect_Bridge.com',
   creator: 'Keith Dockery',
   publisher: 'Aspect Bridge',
-  icons: '/assets/binary2.png',
+  icons: galaxyImg,
   assets: '/assets',
 }
 export const viewport: Viewport = {width: 'device-width', initialScale: 1}
@@ -24,7 +25,7 @@ export default async function RootLayout({children}: {children: React.ReactNode}
         <div id='header' className='row well-sm tcenter logo'>
           <div className='col-xs-12 col-sm-3 col-md-2 col-lg-1 tcenter navy_back title' style={{height: '100%'}}>
             <a href={'/bridge'}>
-              <img src={'https://stsci-opo.org/STScI-01GS6A1YR1W0CXGTPG0VX1FTZA.png'} height={'100px'} style={{borderRadius: '50%'}}/>
+              <img src={galaxyImg} height={'100px'} style={{borderRadius: '50%'}}/>
             </a>
           </div>
           <div className='col-xs-12 col-sm-9 col-md-10 col-lg-11 tcenter navy_back title' style={{height: '100%'}}>
