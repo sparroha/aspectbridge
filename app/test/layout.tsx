4a810548dfd611fd5c869c656cf5a1d077a71afb
Keith <940398+sparroha@users.noreply.github.com>
2024-08-25 11:36:44 -0400

    ,

--- app/test/layout.tsx
+++ app/test/layout.tsx
@@ -16,9 +16,9 @@ export default async function Layout({children}) {
         <br/>
         <hr/>
         <div className='row hauto'>
-          <a href='/test/ident?a=2&b=2'>aspectbridge.com/test/<i>ident?a=2&b=2</i></a><br/>
-          <a href='/test/2?id=2'>aspectbridge.com/test/<i>2?id=2</i></a><br/>
-          <a href='/test/account/2?a=2&b=2'>aspectbridge.com/test/<i>account/2?a=2&b=2</i></a><br/>
+          <a href='/test/ident?a=2&b=2' className={'col'}>aspectbridge.com/test/<i>ident?a=2&b=2</i></a><br/>
+          <a href='/test/2?id=2' className={'col'}>aspectbridge.com/test/<i>2?id=2</i></a><br/>
+          <a href='/test/account/2?a=2&b=2' className={'col'}>aspectbridge.com/test/<i>account/2?a=2&b=2</i></a><br/>
         </div>
   </div>
 }
--- components/debugpageparams.tsx
+++ components/debugpageparams.tsx
@@ -1,76 +1,34 @@
 'use client'
 import { useEffect, useMemo, useState } from "react"
-import Mouse, { useMousePosition } from "./mouse";
+//import Mouse, { useMousePosition } from "./mouse";
 
 export default function DebugParams({params, searchParams}){
     const queryObject = searchParams
     const queryArray: [string, string][] = Object.entries(queryObject);
 
-    const [state, setState] = useState({left: 0, top: 0, dragging: false, drop: false})
-    
-    const [drop, setDrop] = useState(false)
-    
-    const _up = (e, mousepos) => {
-        console.log('upAdd')
-        setState((s)=>{
-            let news={...s}
-            news.dragging = false
-            news.drop = true
-            news.left = mousepos.left
-            news.top = mousepos.top
-            return news
-        })
-        setDrop(true)
-    }
-    const _down = (e) => {
-        console.log('downAdd')
-        setState((s)=>{
-            let news={...s}
-            news.dragging = true
-            return news
-        })
-    }
-    const {mousepos, clickpos, lastClickpos, trate} = useMousePosition({screenid: 'debug', mUp: _up});
-    
     const [debug, setDebug] = useState(false)
     const toggleDebug = ()=>setDebug(!debug)
-    const ToggleDebug = ()=> <button style={{width: 'auto', zIndex: 10}} onClick={toggleDebug}>Toggle Debug</button>
+    const ToggleDebug = ()=> <button style={{width: 'auto'}} onClick={toggleDebug}>Toggle Debug</button>
 
-    const Dragable = useMemo(()=>({children})=>{
-        return <div style={{position: 'absolute', left: state.left, top: state.top, width: 100, height: 100, backgroundColor: 'blue'}} onMouseDown={_down} draggable>
+    const [state, setState] = useState({left: 200, top: 200})
+    function Dragable({state, setState, children}){
+        return <div style={{position: 'absolute', left: state.left, top: state.top, width: 'auto', height: 'auto', backgroundColor: 'blue'}}
+                    onDragEnd={(e)=>{console.log('drag',e);setState((s)=>{return {left: e.pageX, top:e.pageY}})}}>
             <p>Drag me</p>
             {children}
         </div>
-    },[state.left, state.top])
+    }
 
-    useEffect(()=>{
-        if(state.dragging) return
-        if(!state.drop) return
-        setDrop(false)
-
-    } ,[state.dragging])
-
-    if(!debug) return <Dragable><ToggleDebug/>
-        <Mouse id={'debug'} debug={true}/></Dragable>
-    return <Dragable><ToggleDebug/>
-        <Mouse id={'debug'} debug={true}/>
-        <p>params: {JSON.stringify(params)}</p>
-        <p>searchParams: {JSON.stringify(searchParams)}</p>
-        <br/>
-        <p>
-            page: {params.id.map(
-                (s)=>'/'+s
-            )}<br/>
-        </p>
-        <p>
-            query: {queryArray.map(
-                ([id,value])=>id+'='+value
-            ).join('&')}
-        </p>
-        {/*<ul>
-            {queryArray.map(([id,value], key) => {
-                return <li id={id} key={key}>{id+' '+value}</li>
-            })}
-        </ul>*/}
+    return <Dragable state={state} setState={setState}><ToggleDebug/>
+        <div style={{height: 'auto', width: 'auto', visibility: (debug?'visible':'collapse')}}>
+            <p>params: {JSON.stringify(params)}</p>
+            <p>searchParams: {JSON.stringify(searchParams)}</p>
+            <br/>
+            <p>
+                query: {queryArray.map(
+                    ([id,value])=>id+'='+value
+                ).join('&')}
+            </p>
+        </div>
     </Dragable>
 }
