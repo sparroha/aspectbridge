b528dc7a699999a3309d5bd112ec9c14ee0e6ba2
Keith <940398+sparroha@users.noreply.github.com>
2024-08-28 11:58:03 -0400

    .

--- app/api/hello/[...slugs]/route.ts
+++ app/api/hello/[...slugs]/route.ts
@@ -3,7 +3,6 @@ import { getQuery, getSpreadSlugs } from "../../util/params";
 
 export function GET(req: Request, context: any, res: Response){
     const slugs = getSpreadSlugs(context, 'slugs');
-    while (!req) {setTimeout(() => {}, 1000)}
     const query = getQuery(req);
     return NextResponse.json({context: context, query: query, slugs: slugs});
 }
--- app/api/hello/[id]/[name]/route.ts
+++ app/api/hello/[id]/[name]/route.ts
@@ -3,7 +3,9 @@ import { getQuery, getSlugs } from "../../../util/params";
 
 export function GET(req: Request, context: any, res: Response){
     const slugs: {[key: string]: string} = getSlugs(context);
-    const {id, name}: {[key: string]: string} = slugs
+    //const {id, name}: {[key: string]: string} = slugs
+    
+    while (!req) {setTimeout(() => {}, 1000)}
     const query = getQuery(req);
     return NextResponse.json({context: context, query: query, slugs: slugs});
 }
