1adefd470fe1e50aadc17e54f173aa349615d5bf
Keith <940398+sparroha@users.noreply.github.com>
2024-08-28 12:06:26 -0400

    .

--- app/api/hello/[id]/[name]/route.ts
+++ app/api/hello/[id]/[name]/route.ts
@@ -3,9 +3,7 @@ import { getQuery, getSlugs } from "../../../util/params";
 
 export function GET(req: Request, context: any, res: Response){
     const slugs: {[key: string]: string} = getSlugs(context);
-    //const {id, name}: {[key: string]: string} = slugs
-    
-    while (!req) {setTimeout(() => {}, 1000)}
+    const {id, name}: {[key: string]: string} = slugs
     const query = getQuery(req);
     return NextResponse.json({context: context, query: query, slugs: slugs});
 }
