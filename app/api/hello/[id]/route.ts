379dbab3aa4d8d8e693b34ed427e4d9f4cafa5e4
Keith <940398+sparroha@users.noreply.github.com>
2024-08-28 11:39:44 -0400

    .

--- app/api/hello/[id]/route.ts
+++ app/api/hello/[id]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import { getParams, getQuery, getSlug } from "../../util/params";
+import { getQuery, getSlug } from "../../util/params";
 
 export function GET(req: Request, context: any, res: Response){
     const id: string = getSlug(context,'id');
--- app/api/hello/route.ts
+++ app/api/hello/route.ts
@@ -1,10 +1,11 @@
 import { NextResponse } from "next/server";
-import { getSearchParams } from "../util/params";
+import { getQuery, getSearchParams } from "../util/params";
 
 // GET /api/hello?name=World
 export async function GET(req: Request, context: any, res: Response) {
-    const name = getSearchParams(req).get('name') || 'World';
-    return NextResponse.json({ message: 'Hello '+name+'!', context: context, request: req});
+    const query = getQuery(req);
+    const name = query['name'] || 'World';
+    return NextResponse.json({ message: 'Hello '+name+'!', context: context, query: query});
 }
 
 export async function POST(req: Request, res: Response) {
