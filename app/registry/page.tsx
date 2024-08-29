1e740d1b8d677f4301bfd1f6aee017f0c0d30a06
Keith <940398+sparroha@users.noreply.github.com>
2024-08-29 00:16:58 -0400

    .

--- app/registry/page.tsx
+++ app/registry/page.tsx
@@ -1,11 +1,11 @@
 'use client'
-import { useEffect, useMemo, useState } from "react"
+import { useEffect, useState } from "react"
 import { RegistryEntry } from "../api/registry/route"
 import RegistryItem from "./registryitem"
 
 //const ftd = [{order: 0, name: '', type:'', subtype:'', children:<></>, img: wikiImg+'', href: wiki+''},]
 
-export default function AllRegistries({params, searchParams}){
+export default function Page({params, searchParams}){
     
     const app = searchParams.app
     const [registries, setRegistries]: [RegistryEntry[], any] = useState(null)
@@ -19,12 +19,12 @@ export default function AllRegistries({params, searchParams}){
     }, [registries])
 
     return <div style={{backgroundColor: 'white'}}>
-            <div className={'row'}>
-                {registries?.map((registry, i)=>{
+        <div className={'row'}>
+            {registries?.map((registry, i)=>{
                 return <div key={i} className={'col-xs-12 col-sm-12 col-md-6 col-lg-4'} style={{padding: '10px'}}>
-                    <RegistryItem registry={registry}/>
+                    {/*<RegistryItem registry={registry}/>*/}
                 </div>
             })}
-            </div>
+        </div>
     </div>
 }
