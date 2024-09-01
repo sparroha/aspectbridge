16f858170fdd438269aacf0c2e7db71a345a27ad
Keith <anthymngalaris@gmail.com>
2024-09-01 13:01:19 -0400

    gridify in test

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -13,3 +13,18 @@ export default function Page({params, searchParams}){
         <DebugParams params={params} searchParams={searchParams}/>
     </>
 }
+
+function grid(width,height,cell): any[][]{
+    let arr = []
+    ler row = 0
+    for(let y=height;y>0;y--){
+        arr.push([])
+        let col = 0
+        for(let x=0;x<width;x++){
+            arr[row].push(<cell row={row} col={col}/>)
+            col++
+        }
+        row++
+    }
+    return arr
+}
